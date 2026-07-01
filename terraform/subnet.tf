0a8b00c7eb4d2c0745c921d16ef725d82a56a479
amangop-2000 <amangopkv@gmail.com>
2026-07-01 16:38:16 +0530

    Initial Terraform networking (VPC, subnet, IGW, route table)

---
+++ terraform/subnet.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "node-todo-public-subnet"
+  }
+}
