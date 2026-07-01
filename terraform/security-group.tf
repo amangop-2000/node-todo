ba77755162deb1fad65e0b2d521bb64f72efd850
amangop-2000 <amangopkv@gmail.com>
2026-07-01 16:39:12 +0530

    Security Group and IAM

---
+++ terraform/security-group.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "node_todo_sg" {
+  name        = "node-todo-sg"
+  description = "Security group for Node Todo application"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Node App"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "node-todo-sg"
+  }
+}
